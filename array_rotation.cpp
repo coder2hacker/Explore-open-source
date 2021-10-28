ca9806af899845bb4e75c4e3edd7cfef23c57832
ayushjha952 <60105854+ayushjha952@users.noreply.github.com>
2021-10-28 12:34:46 +0530

    Added array_rotation.cpp

---
+++ array_rotation.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void leftRotateArray(int arr[], int r, int n)
+{
+    r = r % n;
+    if (r == 0)
+        return;
+    reverse(arr + 0, arr + r);
+    reverse(arr + r, arr + n);
+    reverse(arr + 0, arr + n);
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int n, i, r;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements " << endl;
+    for (i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Enter the Rotating Factor: ";
+    cin >> r;
+    leftRotateArray(arr, r, n);
+    printArray(arr, n);
+    return 0;
+} 
