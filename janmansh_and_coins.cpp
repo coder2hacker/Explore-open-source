6c2ac48bd6e835fb159a549733bdd417a9532e84
gursewak8642 <82928107+gursewak8642@users.noreply.github.com>
2022-10-05 17:44:23 +0530

    Janmansh and coins using C++

---
+++ janmansh_and_coins.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << ((x * 10) + (y * 5)) << endl;
+    }
+    return 0;
+}
