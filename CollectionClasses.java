ab034e3c957588b5218ed750306f95d53f479219
Subham988 <83450579+Subham988@users.noreply.github.com>
2022-10-09 15:12:38 +0530

    Collection class Using Java

---
+++ CollectionClasses.java
@@ -0,0 +1,49 @@
+
+public class CollectionClasses {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.	AbstractCollection:
+		 * 		Implements most of the collection interface.
+		 * 
+		 * 2.	AbstractList: 
+		 * 		Extends AbstractCollection and implements most of the List interface.
+		 * 
+		 * 3.	AbstractQueue:
+		 *     	Extends AbstractCollection and implements most of the Queue interface.
+		 * 
+		 * 4.  	AbstractSequentialList:
+		 *     	Extends AbstractList for use by a collection that uses sequential rather than
+		 *     	random access of the elements. 
+		 *     
+		 * 5.	LinkedList:
+		 *  	Implements a linked list by extending AbstractSequentialList.
+		 *     
+		 * 6.  	ArrayList: 
+		 *     	Implements a dynamic array by extending AbstractList.
+		 *
+		 * 7.  	ArrayDeque:
+		 *     	Implements a dynamic double ended queue by extending AbstractCollection
+		 *     	and implementing the Deque interface.
+		 *     
+		 * 8.  	AbstractSet:
+		 * 	   	Extends AbstractCollection and implements most of the Set interface.
+		 * 
+		 * 9.  	EnumSet:
+		 *     	Extends AbstractSet for use with enum elements. 
+		 * 
+		 * 10. 	HashSet:
+		 *     	Extends AbstractSet for use with a Hash table.
+		 *     
+		 * 11. 	LinkedHashSet:
+		 *     	Extends HashSet to allow insertion-order iteration.
+		 *     
+		 * 12. 	PriorityQueue:
+		 *     	Extends AbstractQueue to support a priority based queue.
+		 *     
+		 * 13. 	TreeSet:
+		 *     	Implements a Set sorted in a Tree. Extends AbstractSet.
+		 *     
+		 */
+	}
+}
