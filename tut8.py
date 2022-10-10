d2aa4fca8a098eab48d5d8cdb4272365102f5f82
Mihi45 <102919397+Mihi45@users.noreply.github.com>
2022-10-10 21:58:32 +0530

    String

---
+++ tut8.py
@@ -0,0 +1,13 @@
+# index start with 0
+#String slicing
+# print(mystr[4])
+# print(mystr[0:5])
+# print(len(mystr))
+# print(mystr.isalnum())     #0alpha numeric means that stirng which have no spaces
+# print(mystr.endswith("boy"))
+# print(mystr.count("o"))
+# print(mystr.capitalize())
+mystr1= "Mihir is a good boy "
+mystr2 = "He is a Student"
+print(mystr1)
+print(mystr1+mystr2)
