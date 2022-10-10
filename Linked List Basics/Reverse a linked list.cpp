01512abbfcf8edf111df4eb7e5ab5c14081e97be
SWARAJ D SHELAVALE <47442263+SwarajShelavale@users.noreply.github.com>
2022-10-10 20:50:27 +0530

    Create Reverse a linked list.cpp

---
+++ Linked List Basics/Reverse a linked list.cpp
@@ -0,0 +1,90 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+/* Function to get the middle of the linked list*/
+struct Node *reverseList(struct Node *head);
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        head = reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+// Should reverse list and return new head.
+struct Node* reverseList(struct Node *head)
+{
+    if(head==NULL)
+        return head;
+    Node *cur=head,*prev=NULL,*temp=head;
+    while(cur!=NULL)
+    {
+        temp=cur->next;
+        cur->next=prev;
+        prev=cur;
+        cur=temp;
+    }
+    return prev;
+}
