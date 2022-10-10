1a38cfc1def626b59a982797baaec4ed6bea7813
Mihi45 <102919397+Mihi45@users.noreply.github.com>
2022-10-10 21:56:57 +0530

    Factorial Program

---
+++ tut7.py
@@ -0,0 +1,7 @@
+var1= "Hello";   #string
+var2 = 12;
+var3 = 34.5;
+var4 = "World!";
+#if we want to change to int to any data types then we use the following
+print(int(var2) +int(var3));
+print(var1+var4);
