00657e4970820c58a67c2c737d8140ee095c485d
prathamesh792002 <102459080+prathamesh792002@users.noreply.github.com>
2022-10-10 21:26:56 +0530

    If else

---
+++ if else.py
@@ -0,0 +1,29 @@
+#a = int(input("Enter any number"))
+#if a % 2==0 :
+    #print("even")
+#else:
+    #print("odd")
+
+a =int(input("Enter any number :"))
+if a > 0 :
+        print("positive " ,end="")
+        if a % 2==0 :
+            print("even")
+        else: print("odd")
+elif a < 0 :
+        print("negative ", end="")
+        if a % 2==0 :
+            print("even")
+        else: print("odd")
+else:print("zero")
+
+
+
+
+
+
+
+
+
+
+
