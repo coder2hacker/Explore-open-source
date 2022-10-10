c7cd404b47dae097880146129507871b5a941165
Ayush-Shukla <115418426+CoderhuVro@users.noreply.github.com>
2022-10-10 19:28:52 +0530

    Update insertBegin.cpp

--- Linked List Basics/insertBegin.cpp
+++ Linked List Basics/insertBegin.cpp
@@ -15,3 +15,41 @@ int main()
   
     return 0;
 }
+
+
+//Given a Linked List and an Integer x, we need to insert a new node with value x at start of the linked list...
+
+Node *insertBegin(Node *head, int x) {
+    Node *temp = new Node(x);
+    temp->next = head;
+    return temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    head = insertBegin(head, 30);
+    head = insertBegin(head, 20);
+    head = insertBegin(head, 10);
+  
+    return 0;
+}
+
+
+//Given a Linked List and an Integer x, we need to insert a new node with value x at start of the linked list...
+
+Node *insertBegin(Node *head, int x) {
+    Node *temp = new Node(x);
+    temp->next = head;
+    return temp;
+}
+
+int main()
+{
+    Node *head = NULL;
+    head = insertBegin(head, 30);
+    head = insertBegin(head, 20);
+    head = insertBegin(head, 10);
+  
+    return 0;
+}
