2664c0567ed5b9f526ef5852b86db82ada6682e0
omkardb3 <115912556+omkardb3@users.noreply.github.com>
2022-10-16 10:14:03 +0530

    Added Solution for LeetCode Problem No. 22

---
+++ LeetCode/22. Generate Parentheses/Solution.java
@@ -0,0 +1,22 @@
+//
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+         List<String> result = new LinkedList<String>();
+        process("", n, n, result);
+        return result;
+    }
+    
+    public void process(String prefix, int left, int right, List<String> result) {
+        if (left == 0 && right == 0) {
+            result.add(prefix);
+            return;
+        }
+        if (left > 0) {
+            process(prefix + '(', left - 1, right, result);
+        }
+        if (left < right) {
+            process(prefix + ')', left, right - 1, result);
+        }
+    }
+}
