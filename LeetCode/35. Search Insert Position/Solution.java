a9908ff7226f0216bbf88dd91d38e6e570905293
pandurang05 <115934949+pandurang05@users.noreply.github.com>
2022-10-16 19:29:05 +0530

    Added Solution for LeetCode Problem No. 35

---
+++ LeetCode/35. Search Insert Position/Solution.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = end + ( start - end) / 2;
+            if(nums[mid] == target)
+                return mid;
+            
+            if(target < nums[mid]){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
