6cb2dffe65c86162832401f1a105e7660a3d669c
shashank-0-0 <111702455+shashank-0-0@users.noreply.github.com>
2022-10-17 20:07:10 +0530

    Update Solution.java

--- LeetCode/1. Two Sum/Solution.java
+++ LeetCode/1. Two Sum/Solution.java
@@ -13,3 +13,26 @@ class Solution {
         return null;
     }
 }
+//Two pointer method
+//If the array is sorted and you want to check if there exists two numbers adding to a given sum or not.
+class Solution {
+    public boolean twoSum(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int sum=nums[i]+nums[j];
+            if(sum==target){
+                return true;
+            }else if(sum<target){
+                //if the sum is lesser then we increase the i pointer cuz we need a larger value to make the sum.(array is sorted)
+                i++;
+            }else{
+                //if the sum is greater then we decrease the j pointer cuz we need a smaller value.
+                j--;
+            }
+        }
+        return false;
+    }
+}
+Time complexity=O(n)
+space complexity=O(1)
