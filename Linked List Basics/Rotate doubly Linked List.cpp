c3d20a96eeac7bf3437a40e22e802e9b5e154d8f
shelavaleswaraj1610 <116029087+shelavaleswaraj1610@users.noreply.github.com>
2022-10-20 19:35:01 +0530

    Rotate doubly Linked List.cpp

---
+++ Linked List Basics/Rotate doubly Linked List.cpp
@@ -0,0 +1,104 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+struct node* update(struct node*start,int p);
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+        struct node*start = NULL;
+        struct node* cur = NULL;
+        struct node* ptr = NULL;
+        
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            ptr=new node(a);
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=NULL;
+            if(start==NULL)
+            {
+                start=ptr;
+                cur=ptr;
+            }
+            else
+            {
+                cur->next=ptr;
+                ptr->prev=cur;
+                cur=ptr;
+            }
+        }
+        struct node*str=update(start,p);
+        while(str!=NULL)
+        {
+            cout<<str->data<<" ";
+            str=str->next;
+        }
+        cout<<endl;  
+    }
+}
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+/*
+struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+};
+*/
+
+struct node*update(struct node*start,int p)
+{
+    //Add your code here
+    node*temp=start,*last=start;
+    while(last->next!=NULL)
+    {
+        last=last->next;
+    }
+    int count=0;
+    while(count!=p)
+    {
+        temp=temp->next;
+        count++;
+    }
+    start->prev=last;
+    last->next=start;
+    start=temp;
+    start->prev->next=NULL;
+    start->prev=NULL;
+    
+    return start;
+    
+}
+
+// { Driver Code Starts.
+  // } Driver Code Ends
