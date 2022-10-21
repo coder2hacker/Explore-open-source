488c43a508e65d7f9b741f80d4020115098ecb3d
omkar2508 <116364228+omkar2508@users.noreply.github.com>
2022-10-21 19:10:32 +0530

    Added Solution for LeetCode Problem No. 71

---
+++ LeetCode/71. Simplify Path/Solution.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/simplify-path/
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> s = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        String[] p =path.split("/");
+        
+        for(int i=0;i<p.length;i++){
+            if(!s.isEmpty()  && p[i].equals("..")) s.pop();
+            else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+                s.push(p[i]);
+        }
+        
+        if(s.isEmpty()) return "/";
+        while(!s.isEmpty()){
+            res.insert(0,s.pop()).insert(0,"/");
+        }
+        
+        return res.toString();
+    }
+}
