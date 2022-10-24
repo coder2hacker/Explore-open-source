2e8c587ce2b4dc347ab165d721612c626086f8ac
Sanjay8488 <116538119+Sanjay8488@users.noreply.github.com>
2022-10-24 10:20:22 +0530

    Added Solution for LeetCode Problem No. 92

---
+++ 92. Reverse Linked List II/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0); // created dummy node
+        dummy.next = head;
+        ListNode prev = dummy; // intialising prev pointer on dummy node
+        
+        for(int i = 0; i < left - 1; i++)
+            prev = prev.next; // adjusting the prev pointer on it's actual index
+        
+        ListNode curr = prev.next; // curr pointer will be just after prev
+        // reversing
+        for(int i = 0; i < right - left; i++){
+            ListNode forw = curr.next; // forw pointer will be after curr
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
+        }
+        return dummy.next;
+    }
+}
