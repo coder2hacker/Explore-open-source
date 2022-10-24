c3fdbcd097d692c469b742d629180fda336ae12e
kiranb5050 <116535544+kiranb5050@users.noreply.github.com>
2022-10-24 09:03:24 +0530

    Added Solution for LeetCode Problem No. 84

---
+++ LeetCode/84. Largest Rectangle in Histogram/Solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        if(n == 0) return 0; // Base Condition
+        int maxArea = 0;
+        int left[] = new int[n]; //fill left boundary
+        int right[] = new int[n]; // fill right boundary
+        
+        left[0] = -1;
+        right[n - 1] = n;
+        
+        for(int i = 1; i < n; i++){
+            int prev = i - 1; // previous for comparing the heights
+            while(prev >= 0 && heights[prev] >= heights[i]){
+                prev = left[prev]; // we have done this to minimise the jumps we make to the left
+            }
+            left[i] = prev;
+        }
+        // Similarly we do for right
+        for(int i = n - 2; i >= 0; i--){
+            int prev = i + 1; 
+            while(prev < n && heights[prev] >= heights[i]){
+                prev = right[prev]; 
+            }
+            right[i] = prev;
+        }
+        // once we have these two arrays fill we need width & area
+        for(int i = 0; i < n; i++){
+            int width = right[i] - left[i] - 1;
+            maxArea = Math.max(maxArea, heights[i] * width);
+        }
+        return maxArea;
+        
+    }
+}
